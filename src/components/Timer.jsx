1a9b202ea658760f3d7e2a4896f232c96ca6eaba
Gerardo Huerta <gerardoh13@gmail.com>
2022-07-16 20:33:54 -0700

    downgraded to React 17

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import { Card, Button, Col, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDown, faArrowUp, faCirclePlay, faCirclePause, faArrowRotateBackward } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown, faArrowUp, faCirclePlay, faCirclePause, faArrowRotateBackward, faSeedling } from '@fortawesome/free-solid-svg-icons'
 import accurateInterval from "../accurate_interval";
 
 
 class Timer extends React.Component {
     state = {
-        breakLen: 5,
+        breakLen: 5, 
         seshLen: 25,
         paused: true,
         time: 1500,
-        session: 'Session',
+        session: true,
         clock: ''
     }
 
     clearClock = () => {
-            if (this.state.clock){
+        const { clock } = this.state
+            if (clock){
                 this.state.clock.cancel()
-                this.setState(() =>{
-                    return {
+                this.setState({
                         clock: ''
-                    }
                 })
             }
     }
     tick = () => {
         const { paused, time } = this.state
-        if (paused || time < 0){
+        if (time === 0){
+            this.switchTimer()
+        }
+        if (paused || time === 0){
             return
         } else {
             this.setState(prevState => {
@@ -44,50 +46,36 @@ class Timer extends React.Component {
 
     startPause = () => {
         const { paused } = this.state
-        console.log('playPause', paused)
         if (paused){
-            this.setState( prevState => {
-                return {
-                    paused: !prevState.paused,
-                    clock: accurateInterval(() => {this.tick(); this.switchTimer()}, 1000)
-                }
+            this.setState({
+                    paused: false,
+                    clock: accurateInterval(this.tick, 1000)
             })
         } else {
-            if (this.state.clock){
-                this.state.clock.cancel()
-            }
-            this.setState( prevState => {
-                return {
-                    paused: !prevState.paused,
-                    clock: ''
-                }
+            this.clearClock()
+            this.setState({
+                    paused: true,
             })
         }
     }
 
 switchTimer = () => {
-    const { time, seshLen, breakLen, session } = this.state
-    if (time === 0) {
+    const { seshLen, breakLen, session } = this.state
         this.alarm()
         this.clearClock()
-        if (session === 'Session'){
-            this.setState(() => {
-                return {
+        if (session){
+            this.setState({
                     time: breakLen * 60,
-                    session: 'Break',
-                    clock: accurateInterval(() => {this.tick(); this.switchTimer()}, 1000)
-                }
+                    session: false,
+                    clock: accurateInterval(this.tick, 1000)
             })
         } else {
-            this.setState(() => {
-                return {
+            this.setState({
                     time: seshLen * 60,
-                    session: 'Session',
-                    clock: accurateInterval(() => {this.tick(); this.switchTimer()}, 1000)
-                }
+                    session: true,
+                    clock: accurateInterval(this.tick, 1000)
             })
         }
-    }
 }
 
     toClock = () => {
@@ -98,226 +86,127 @@ switchTimer = () => {
         return mins + ':' + secs
     }
 
-    breakDec = () => {
-        console.log('breakDec')
-        const { paused, breakLen, session } = this.state
-        if(false){
+    handleBreak = (e) => {
+        let val = e.currentTarget.value
+        this.changeLength(val, 'breakLen')
+    }
+    handleSesh = (e) => {
+        let val = e.currentTarget.value
+        this.changeLength(val, 'seshLen')
+    }
+    changeLength = (val, timer) => {
+        const { paused, session } = this.state
+        if (!paused){
             return
         } else {
-            if (breakLen !== 1){
+            if (val === '-'){
                 this.setState(prevState => {
-                    if (prevState.breakLen === 1){
+                    if (prevState[timer] === 1){
                         return
-                    } else {
-                        return {
-                            breakLen: prevState.breakLen - 1
-                        }
+                    } else 
+                    return {
+                        [timer]: prevState[timer] - 1,
                     }
                 })
-                if (session === 'Break'){
-                    this.setState(prevState => {
-                        return {
-                            time: prevState.breakLen * 60
-                        }
-                    })
-                }
-            }
-        }
-    }
-    breakInc = () => {
-        console.log('breakInc')
-        const { paused, session } = this.state
-        if (false){
-            return 
-        } else {
-            this.setState(prevState => {
-                if (prevState.breakLen === 60){
-                    return
-                } else {
-                    return {
-                        breakLen: prevState.breakLen + 1
-                    }
-                }
-            })
-            if (session === 'Break'){
+            } 
+            if (val === '+'){
                 this.setState(prevState => {
-                    return {
-                        time: prevState.breakLen * 60 + 60
-                    }
-                })
-            }
-        }
-    }
-    seshDec = () => {
-        console.log('seshDec')
-        const { paused, session } = this.state
-        if (false){
-            return 
-        } else {
-                this.setState(prevState => {
-                    if (prevState.seshLen === 1){
+                    if (prevState[timer] === 60){
                         return
-                    } else {
-                        return {
-                            seshLen: prevState.seshLen - 1
-                        }
+                    } else
+                    return {
+                        [timer]: prevState[timer] + 1
                     }
                 })
-                if (session === 'Session'){
-                    this.setState(prevState => {
-                        return {
-                            time: prevState.seshLen * 60
-                        }
-                    })
-                }
             }
-    }
-    seshInc = () => {
-        console.log('seshInc')
-        const { paused, session } = this.state
-        if (false){
-            return 
-        } else {
-            this.setState(prevState => {
-                if (prevState.seshLen === 60){
-                    return 
-                } else {
-                    return {
-                        seshLen: prevState.seshLen + 1
-                    }
-                }
-            })
-            if (session === 'Session'){
+            if (session && timer === 'seshLen'){
                 this.setState(prevState => {
                     return {
-                        time: prevState.seshLen * 60
+                        time: prevState[timer] * 60
+                    }
+                })
+            }
+            if (!session && timer === 'breakLen'){
+                this.setState(prevState => {
+                    return {
+                        time: prevState[timer] * 60
                     }
                 })
             }
         }
     }
-
     reset = () => {
-        console.log('reset')
         this.clearClock()
-        this.setState(() => {
-            return {
+        this.setState({
                 breakLen: 5,
                 seshLen: 25,
                 paused: true,
                 time: 1500,
-                session: 'Session',
-            }
+                session: true,
         })
         this.alarmPlay.pause();
         this.alarmPlay.currentTime = 0;
-    }
-    logState = () => {
-        console.log(this.state)
-    }
-
-    loop = () => {
-        for (let i = 0; i < 36; i++){
-            this.seshInc()
-        }
-    }
-
-    loop2 = () => {
-        for (let i = 0; i < 60; i++){
-            this.seshDec()
-        }
-    }
-    loop3 = () => {
-        for (let i = 0; i < 60; i++){
-            this.breakDec()
-        }
-    }
-    loop4 = () => {
-        for (let i = 0; i < 60; i++){
-            this.breakInc()
-        }
-    }
-    loop5 = () => {
-        for(let i = 0; i<9;i++){
-            this.reset()
-        }
-        for(let i = 0; i<35;i++){
-            this.seshInc()
-        }
-        for(let i = 0; i<3;i++){
-            this.reset()
-        }
-        for(let i = 0; i<60;i++){
-            this.seshDec()
-        }
-        for(let i = 0; i<60;i++){
-            this.breakDec()
-        }
-        this.startPause()
-        this.reset()
-        this.reset()
-        for(let i = 0; i<4;i++){
-            this.breakDec()
-        }
-        for(let i = 0; i<2;i++){
-            this.reset()
-        }
-        for(let i = 0; i<60;i++){
-            this.seshDec()
-        }
     }
 
     render () {
         return (
             <div>
-                <Card>
-                    <Row>
+                <FontAwesomeIcon icon={faSeedling} size="5x" className="stem"/>
+                <Card className="card">
+                    <Card.Header>
+                    <h3 id="timer-label">{this.state.session ? "Session" : "Break"}</h3>
+                    </Card.Header>
+                    <Card.Body className="display">
+                    <h1 id="time-left">{this.toClock()}</h1>
+                    </Card.Body>
+                <Row className="mt-4">
+                    <Col>
+                    <Button id="start_stop" variant={this.state.paused ? "success" : "secondary"} onClick={this.startPause}>{this.state.paused ? <FontAwesomeIcon icon={faCirclePlay} /> : <FontAwesomeIcon icon={faCirclePause} />}</Button>
+                    </Col>
+                    <Col>
+                    <Button id="reset" variant="warning" onClick={this.reset}><FontAwesomeIcon icon={faArrowRotateBackward} /></Button>
+                    </Col>
+                </Row>
+                    <Row className="mt-4">
                         <Col>
-                        <h4 id="break-label">Break Length</h4>
+                        <h4 id="break-label">Break</h4>
                         </Col>
                         <Col>
-                        <h4 id="session-label">Session Length</h4>
+                        <h4 id="session-label">Session</h4>
                         </Col>
                     </Row>
-                    <br/>
                     <Row>
-                        <Col>
-                        <Button id="break-decrement" value={'-'} onClick={this.breakDec}><FontAwesomeIcon icon={faArrowDown} /></Button>
-                        </Col>
                         <Col>
                         <h4 id="break-length">{this.state.breakLen}</h4>
                         </Col>
                         <Col>
-                        <Button id="break-increment" value={'+'} onClick={this.breakInc}><FontAwesomeIcon icon={faArrowUp} /></Button>
-                        </Col>
-                        <Col>
-                        <Button id="session-decrement"value={'-'} onClick={this.seshDec}><FontAwesomeIcon icon={faArrowDown} /></Button>
-                        </Col>
-                        <Col>
                         <h4 id="session-length">{this.state.seshLen}</h4>
                         </Col>
+                    </Row>
+                    <Row>
+                        <Col >
+                        <span className="pad">
+                        <Button id="break-decrement" variant="dark" value={'-'} onClick={this.handleBreak}><FontAwesomeIcon icon={faArrowDown} /></Button>
+                        </span>
+                        <span className="pad">
+                        <Button id="break-increment" variant="dark" value={'+'} onClick={this.handleBreak}><FontAwesomeIcon icon={faArrowUp} /></Button>
+                        </span>
+                        </Col>
                         <Col>
-                        <Button id="session-increment" value={'+'}onClick={this.seshInc}><FontAwesomeIcon icon={faArrowUp} /></Button>
+                        <span className="pad">
+                        <Button id="session-decrement" variant="dark" value={'-'} onClick={this.handleSesh}><FontAwesomeIcon icon={faArrowDown} /></Button>
+                        </span>
+                        <span className="pad">
+                        <Button id="session-increment" variant="dark" value={'+'} onClick={this.handleSesh}><FontAwesomeIcon icon={faArrowUp} /></Button>
+                        </span>
+
                         </Col>
                     </Row>
-                <p id="timer-label">{this.state.session}</p>
-                <h1 id="time-left">{this.toClock()}</h1>
-                <Row>
-                    <Col>
-                    <Button id="start_stop" onClick={this.startPause}>{this.state.paused ? <FontAwesomeIcon icon={faCirclePlay} /> : <FontAwesomeIcon icon={faCirclePause} />}</Button>
-                    </Col>
-                    <Col>
-                    <Button id="reset" onClick={this.reset}><FontAwesomeIcon icon={faArrowRotateBackward} /></Button>
-                    </Col>
-                </Row>
-                <Button onClick={this.logState}>state</Button>
-                <Button onClick={this.loop}>loop</Button>
-                <Button onClick={this.loop2}>loop2</Button>
-                <Button onClick={this.loop3}>loop3</Button>
-                <Button onClick={this.loop4}>loop4</Button>
-                <Button onClick={this.loop5}>loop5</Button>
-
+                    <br/>
                 </Card>
+                <br/>
+                <h5 className="footer">Pomodoro Clock</h5>
+                <h5 className="footer">by gerardoh13</h5>
                 <audio id="beep" preload="auto" ref={(audio) => {this.alarmPlay = audio; }}src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"/>
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -7,12 +8,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
-    <App />
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+ReactDOM.render(
+  // <React.StrictMode> 
+    <App />, container
   /* </React.StrictMode> */
-
 );
 
 // If you want to start measuring performance in your app, pass a function
